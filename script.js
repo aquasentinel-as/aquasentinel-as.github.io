e31589d6dc7945c425d825d791bd998a6f825647
aquasentinel-as <info.aquasentinel.as@gmail.com>
2025-11-04 22:16:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,11 @@ if (form) {
     form.reset();
   });
 }
-function toggleMenu() {
-  document.querySelector(".nav-links").classList.toggle("active");
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
 
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+});
